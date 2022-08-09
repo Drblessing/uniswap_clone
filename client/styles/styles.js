41185965fc66a294b62afc6957b6a61ab9d33f4b
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-08-09 10:56:13 -0700

    init

---
+++ client/styles/styles.js
@@ -0,0 +1,50 @@
+// Index
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+};
+
+// Transaction Loader
+const style = {
+  wrapper: `text-white h-96 w-72 flex flex-col justify-center items-center`,
+  title: `font-semibold text-xl mb-12`,
+};
+
+// Transaction History
+const style = {
+  wrapper: `h-full text-white select-none h-full w-screen flex-1 pt-14 flex items-end justify-end pb-12 overflow-scroll px-8`,
+  txHistoryItem: `bg-[#191a1e] rounded-lg px-4 py-2 my-2 flex items-center justify-end`,
+  txDetails: `flex items-center`,
+  toAddress: `text-[#f48706] mx-2`,
+  txTimestamp: `mx-2`,
+  etherscanLink: `flex items-center text-[#2172e5]`,
+};
+
+// Main
+const style = {
+  wrapper: `w-screen flex items-center justify-center mt-14`,
+  content: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
+  formHeader: `px-2 flex items-center justify-between font-semibold text-xl`,
+  transferPropContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
+  transferPropInput: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
+  currencySelector: `flex w-1/4`,
+  currencySelectorContent: `w-full h-min flex justify-between items-center bg-[#2D2F36] hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]`,
+  currencySelectorIcon: `flex items-center`,
+  currencySelectorTicker: `mx-2`,
+  currencySelectorArrow: `text-lg`,
+  confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
+};
+
+// Header
+const style = {
+  wrapper: `w-screen flex items-center justify-center mt-14`,
+  content: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
+  formHeader: `px-2 flex items-center justify-between font-semibold text-xl`,
+  transferPropContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
+  transferPropInput: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
+  currencySelector: `flex w-1/4`,
+  currencySelectorContent: `w-full h-min flex justify-between items-center bg-[#2D2F36] hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]`,
+  currencySelectorIcon: `flex items-center`,
+  currencySelectorTicker: `mx-2`,
+  currencySelectorArrow: `text-lg`,
+  confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
+};
