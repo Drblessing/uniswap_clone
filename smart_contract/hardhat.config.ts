3f1c173db9d40cbc8d39dc0ce693ccb2fd5e74c3
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-08-10 09:26:17 -0700

    hardhat config

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,18 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'dotenv/config'
+import "@nomiclabs/hardhat-etherscan"
 
 
-const privateKey: string = process.env.PRIVATE_KEY ?? "default"
-
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? "default"
+const POLYGON_SCAN: string = process.env.POLYGON_SCAN_API_KEY ?? "default"
 const config: HardhatUserConfig = {
   defaultNetwork: "matic",
   networks: {
     hardhat: {},
     matic: {
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [privateKey]
+      accounts: [PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: POLYGON_SCAN
   },
   solidity: "0.8.9",
 }
