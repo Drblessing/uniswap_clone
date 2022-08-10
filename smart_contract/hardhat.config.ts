da9c8233f4db631fa353f6c5c65ef326d8f7c944
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-08-09 20:56:40 -0700

    contracts

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
+
+
+const privateKey: string = process.env.PRIVATE_KEY ?? "default"
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.9",
-};
+}
 
 export default config;
